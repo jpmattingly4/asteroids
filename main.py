05617e260984358a25312418c4949eaaa864b088
jpmattingly4 <jpmattingly4@gmail.com>
2025-05-26 11:16:42 -0700

    create window for game

---
+++ main.py
@@ -0,0 +1,25 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughtout this file
+import pygame
+from constants import *
+
+def main():
+    pygame.init() # initializes all imported pygame modules
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    # sets GUI window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        # functionality for windows close button
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        pygame.Surface.fill(screen, (0, 0, 0), None, 0)
+        pygame.display.flip()
+        
+if __name__ == "__main__":
+    main()
